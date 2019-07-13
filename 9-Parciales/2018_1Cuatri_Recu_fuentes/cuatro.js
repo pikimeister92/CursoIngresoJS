bcf3bec721232ce92e577abe7dd5d68d587666bd
Francisco Javier Miranda <52511483+pikimeister92@users.noreply.github.com>
2019-07-13 12:33:33 -0300

    tp
    modificando tengo que aprender a manejar la consola de git

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,41 @@
 function mostrar()
 {
+        var numeroUno;
+        var numeroDos;
+        
+             
+            numeroUno=prompt("numeroUno");
+            numeroDos=prompt ("numeroDos");
+                
+            
+            
+            
+            if(numeroUno== numeroDos)
+                 {
 
-}
+                    alert ((numeroUno + numeroDos));
+                 }
+                 
+                 else   {   if (numeroUno >= numeroDos)
+
+                    alert((numeroUno / numeroDos));
+
+
+                }    
+                
+                
+                
+                
+                
+                
+                }
+
+
+
+
+
+
+        }
+
+
+
